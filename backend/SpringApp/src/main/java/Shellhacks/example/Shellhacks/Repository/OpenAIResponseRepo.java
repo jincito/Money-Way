784dac4c7f3f5b087a53cabe09d760c2db3acf7a
Diego Valero <126991857+diegovaleroo@users.noreply.github.com>
2024-09-29 15:43:23 -0400

    Delete backend directory

--- backend/SpringApp/src/main/java/Shellhacks/example/Shellhacks/Repository/OpenAIResponseRepo.java
+++
@@ -1,8 +0,0 @@
-package Shellhacks.example.Shellhacks.Repository;
-
-import Shellhacks.example.Shellhacks.Model.OpenAIResponse;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OpenAIResponseRepo extends CrudRepository<OpenAIResponse, Integer> {
-    OpenAIResponse findByResponseId(String responseId);
-}
